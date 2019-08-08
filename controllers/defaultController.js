1d3c31b615b31b246b5fe4a9e3501c63c0239cd0
usrcdimi <usrcdimi@cibertec.edu.pe>
2019-08-08 15:45:08 -0500

    register user

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,5 +1,7 @@
 const Post = require('../models/Posts/Post');
 const Category = require('../models/Category.model');
+const User = require('../models/User.model');
+var bcrypt = require('bcryptjs');
 
 module.exports = {
     index :  async (req, res) => {
@@ -18,7 +20,51 @@ module.exports = {
     registerGet: (req, res) => {
        res.render('default/register')
     },
-    registerPost: (req, res) => {
-        res.send('register post');
+    registerPost: async (req, res) => {
+        // users after express-validation
+        let errors = [];
+
+        const { firstName, lastName, email, password, passwordConfirm } = req.body;
+
+        if(!firstName) {
+            errors.push({ message: 'First name is required'});
+        }
+        if(!lastName) {
+            errors.push({ message: 'Last name is required'});
+        }
+        if(!email) {
+            errors.push({ message: 'Email name is required'});
+        }
+        if(!password && password !== passwordConfirm) {
+            errors.push({ message: 'Password do not match'});
+        }
+
+        // if errors
+        if(errors.length > 0) {
+            res.render('default/register', { errors, firstName, lastName, email});
+        } else {
+            const user = await User.findOne({ email });
+            
+            // if existing user in db
+            if(user) {
+                req.flash('error-message', 'Email already exists, try to login');
+                res.redirec('/login');
+            } else {
+                // create new user
+                const newUser = await  new User(req.body);
+
+                // usin bcrypt
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        newUser.password = hash;
+                        await newUser.save();
+                        // sending message OK!
+                        req.flash('success-message', 'You are now Registered!');
+                        res.redirect('/login');
+                    });
+                });
+            }
+            
+        }
     }
 }
