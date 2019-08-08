43c5e7b03bc40ceffbe411dd9ee0545159cc86ce
usrcdimi <usrcdimi@cibertec.edu.pe>
2019-08-08 16:39:34 -0500

    using passport bcrydt && inspector validation routes

--- routes/defaultRouter.js
+++ routes/defaultRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const  defaultController = require('../controllers/defaultController');
 const User = require('../models/User.model');
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
 
@@ -16,10 +16,54 @@ router.all('/*', (req, res, next) => {
 router.route('/')
         .get(defaultController.index);
 
+//defining local strategy
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+}, (req, email, password, done) => {
+        const user = User.findOne({ email });
+        //validation if existing user
+        if(!user) {
+          return done(null, false, req.flash('error_message', 'User not found with this email'));
+        }
+        bcrypt.compare(password, user.password, (error, passwordMatched) => {
+                if(error) {
+                        throw error;
+                }
+                if (!passwordMatched) {
+                        return done(null, false, req.flash('error_message', 'Invalid User or Password'));
+                }
+                // All correct
+                return done(null, user, req.flash('success_message', 'Login Successfully!'));
+        });
+}));
+
+passport.serializeUser(function(user, done) {
+        done(null, user.id);
+});
+      
+passport.deserializeUser(function(id, done) {
+       User.findById(id, function(err, user) {
+       done(err, user);
+        });
+});
+
+// end defining local strategy
+
+
+// login
 router.route('/login')
         .get(defaultController.loginGet)
-        .post(defaultController.loginPost);
+        .post(passport.authenticate('local', {
+                successRedirect: '/admin',
+                failureRedirect: '/login',
+                failureFlash: true,
+                successFlash: true,
+                session: true
+        }) , defaultController.loginPost);
 
+//register
 router.route('/register')
         .get(defaultController.registerGet)
         .post(defaultController.registerPost);
