fac2b38f5da5493f71b35c179ead10d13ffc9576
usrcdimi <usrcdimi@cibertec.edu.pe>
2019-08-12 13:56:47 -0500

    conf template var user condition

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     mongoUrlNative: 'mongodb://localhost:27017/cms-node',
     mongoServiceMlab: 'mongodb://datacms:datacms2019@ds347367.mlab.com:47367/datacms',
     PORT: process.env.PORT || 3000,
-    globalVariables: (err, req, res, next) => {
+    globalVariables: (req, res, next) => {
         res.locals.success_message = req.flash('success_message');
         res.locals.error_message = req.flash('error_message');
         res.locals.user = req.user || null;
